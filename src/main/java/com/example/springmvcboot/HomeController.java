a3cc149f5bc1d2c1e03622f557acbfee78e5f7c0
Thanmayee167 <thanmayee167@gmail.com>
2025-03-04 11:56:23 +0530

    MODEL AND VIEW Map

--- src/main/java/com/example/springmvcboot/HomeController.java
+++ src/main/java/com/example/springmvcboot/HomeController.java
@@ -1,26 +1,24 @@
 package com.example.springmvcboot;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 
-	@GetMapping("/home")
-	public String home() {
-		return "index";
-	}
+  @GetMapping("/home")
+  public String home() {
+    return "index";
+  }
 
-	@GetMapping("/add")
-	public ModelAndView add(@RequestParam("num1") int i, @RequestParam("num2") int j) {
-		ModelAndView mv = new ModelAndView();
-		mv.setViewName("result");
-		int sum = i + j;
+  @GetMapping("/add")
+  public String add(@RequestParam("num1") int i, @RequestParam("num2") int j, ModelMap modelMap) {
+    int sum = i + j;
 
-		mv.addObject("sum", sum);
+    modelMap.addAttribute("sum", sum);
 
-		return mv;
-	}
+    return "result";
+  }
 }
